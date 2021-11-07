42a5fc19c975f515f76201ac4f7e4ebc5bb31fbb
youssefsoli <youssefdevelops@gmail.com>
2021-11-07 01:45:13 -0500

    Fixed tab size to 2

--- Frontend/src/components/CaseList.jsx
+++ Frontend/src/components/CaseList.jsx
@@ -3,39 +3,41 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import IconButton from '@mui/material/IconButton';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import CaseCard from './CaseCard'
+import CaseCard from './CaseCard';
 
 export default function CaseList({ users }) {
-    const [currentUser, setCurrentUser] = useState({})
+  const [currentUser, setCurrentUser] = useState({});
 
-    const showCard = (id) => {
-        fetch(`/api/user/${id}`)
-            .then(response => response.json())
-            .then(user => setCurrentUser(user))
-            .catch(err => {
-                // handle the error
-            })
-    }
+  const showCard = id => {
+    fetch(`/api/user/${id}`)
+      .then(response => response.json())
+      .then(user => setCurrentUser(user))
+      .catch(err => {
+        // handle the error
+      });
+  };
 
-    return (
-        <List dense sx={{ width: '100%', maxWidth: 400, bgcolor: 'gray' }}>
-        {users.map((user) => {
-            return (
-                <>
+  return (
+    <List dense sx={{ width: '100%', maxWidth: 400, bgcolor: 'gray' }}>
+      {users.map(user => {
+        return (
+          <>
             <ListItem
-                key={user.id}
-                secondaryAction={
-                    <IconButton edge="end" aria-label="show-card">
-                    <ArrowDropDownIcon onClick={() => showCard(user.id)}/>
-                    </IconButton>
-                }
+              key={user.id}
+              secondaryAction={
+                <IconButton edge="end" aria-label="show-card">
+                  <ArrowDropDownIcon onClick={() => showCard(user.id)} />
+                </IconButton>
+              }
             >
-                {user.name}
+              {user.name}
             </ListItem>
-            {currentUser.id === user.id ? <CaseCard user={currentUser}></CaseCard> : null}
-            </>
-            );
-        })}
-        </List>
-    );
-    }
+            {currentUser.id === user.id ? (
+              <CaseCard user={currentUser}></CaseCard>
+            ) : null}
+          </>
+        );
+      })}
+    </List>
+  );
+}
