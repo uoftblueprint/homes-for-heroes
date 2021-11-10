c339b5066a58b3b2f725742d232dc3736b9b9710
EclipseIsDead <sidmcheath@gmail.com>
2021-11-10 18:48:53 -0500

    Made component function, sized card

--- Frontend/src/components/CaseCard.js
+++ Frontend/src/components/CaseCard.js
@@ -5,32 +5,27 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-class CaseCard extends React.Component{
-
-    render() {
-        return (
-          <Card sx={{ minWidth: 275 }}>
-            <CardContent>
-              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-               {this.props.user.name}
-              </Typography>
-              <Typography variant="h5" component="div">
-                Notes:
-              </Typography>
-              <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                {this.props.user.notes}
-              </Typography>
-              <Typography variant="body2">
-                {/* Created {this.props.date} by {this.props.signature} */}
-                Created at this time by this admin
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
-        );
-    }
+export default function CaseCard (props) {
+  return (
+    <Card sx={{ minWidth: 275, maxWidth: 550 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+         {props.user.name}
+        </Typography>
+        <Typography variant="h5" component="div">
+          Notes:
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          {props.user.notes}
+        </Typography>
+        <Typography variant="body2">
+          {/* Created {props.date} by {props.çsignature} */}
+          Created at this time by this admin
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
 }
-
-export default CaseCard;
