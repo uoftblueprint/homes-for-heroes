6dee048db012060551ccc968adebc10cf00e21ea
Mesbah Rafin <42284448+dragonite20@users.noreply.github.com>
2021-11-13 08:10:39 -0800

    Update user_schema.sql

--- schemas/user_schema.sql
+++ schemas/user_schema.sql
@@ -9,23 +9,24 @@ CREATE TABLE UserInfo (
     gender CHAR(255) NOT NULL,
     email CHAR(255) NOT NULL,
     applicant_phone INT NOT NULL,
-    application_phone INT NOT NULL,
-    applicant_dob CHAR(255),
+    applicant_dob DATE ,
     street_name CHAR(255),
     city CHAR(255) NOT NULL,
     province CHAR(255) NOT NULL,
     referral CHAR(255),
-    CONSTRAINT FOREIGN KEY (user_id) REFERENCES User(user_id)
+    CONSTRAINT FOREIGN KEY (user_id) REFERENCES client_users(user_id)
 );
 
 DROP TABLE IF EXISTS NextKin;
-CREATE TABLE NextKin (
+CREATE TABLE NextKinInfo (
     kin_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
     kin_name CHAR(255) NOT NULL,
     relationship CHAR(255),
+    kin_phone CHAR(255),
+    kin_email CHAR(255),
     kin_address CHAR(255),
-	CONSTRAINT FOREIGN KEY (user_id) REFERENCES User(user_id)
+	CONSTRAINT FOREIGN KEY (user_id) REFERENCES client_users(user_id)
 );
 
 DROP TABLE IF EXISTS MedicalHistory;
@@ -52,5 +53,5 @@ CREATE TABLE MedicalHistory (
     mental_health_diagnosis BOOLEAN,
     mobility_issues BOOLEAN,
     other_issues BOOLEAN,
-    CONSTRAINT FOREIGN KEY (user_id) REFERENCES User(user_id)
+    CONSTRAINT FOREIGN KEY (user_id) REFERENCES client_users(user_id)
 );
