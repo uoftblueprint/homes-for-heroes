16a6f8c9385530a784fb884ce3ddaeb5594f201b
Mesbah Rafin <42284448+dragonite20@users.noreply.github.com>
2021-11-13 08:02:46 -0800

    Update admin_client_schema.sql

--- schemas/admin_client_schema.sql
+++ schemas/admin_client_schema.sql
@@ -19,16 +19,16 @@ name VARCHAR(255) NOT NULL UNIQUE,
 email VARCHAR(255) NOT NULL UNIQUE,
 phone VARCHAR(255) NOT NULL UNIQUE,
 password VARCHAR(255) NOT NULL,
-client_id int NOT NULL AUTO_INCREMENT,
-primary key (client_id)
+user_id int NOT NULL AUTO_INCREMENT,
+primary key (user_id)
 );
 
 DROP TABLE IF EXISTS cases;
 CREATE TABLE IF NOT EXISTS cases (
-client_id int NOT NULL UNIQUE,
+user_id int NOT NULL UNIQUE,
 admin_id int NOT NULL UNIQUE,
 notes VARCHAR(255),
 last_update DATETIME,
-CONSTRAINT FOREIGN KEY (client_id) REFERENCES client_users(client_id),
+CONSTRAINT FOREIGN KEY (user_id) RuserEFERENCES client_users(user_id),
 CONSTRAINT FOREIGN KEY (admin_id) REFERENCES admin_users(admin_id)
 );
