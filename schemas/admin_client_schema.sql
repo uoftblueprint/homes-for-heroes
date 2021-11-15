4e6d6db1a2c0f583cef9a8f1e2f5fe80f4175ed6
Mesbah Rafin <42284448+dragonite20@users.noreply.github.com>
2021-11-14 18:55:03 -0800

    Update admin_client_schema.sql

--- schemas/admin_client_schema.sql
+++ schemas/admin_client_schema.sql
@@ -28,7 +28,5 @@ CREATE TABLE IF NOT EXISTS cases (
 user_id int NOT NULL UNIQUE,
 admin_id int NOT NULL UNIQUE,
 notes VARCHAR(255),
-last_update DATETIME,
-CONSTRAINT FOREIGN KEY (user_id) REFERENCES client_users(user_id),
-CONSTRAINT FOREIGN KEY (admin_id) REFERENCES admin_users(admin_id)
+last_update DATETIME
 );
