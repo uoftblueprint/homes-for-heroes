4b04c62be53ff96ffb0a2374c7d0931a5db042b4
nancytaen <taen.heizo8@gmail.com>
2021-11-15 22:58:23 -0500

    retrieve all users from client_user table

--- Backend/app/models/customer.model.js
+++ Backend/app/models/customer.model.js
@@ -2,10 +2,24 @@ const sql = require("./db.js");
 
 // constructor
 const Customer = function(customer) {
-  this.email = customer.email;
+  this.user_id = customer.user_id;
   this.name = customer.name;
-  this.active = customer.active;
+  this.email = customer.email;
+  this.phone = customer.phone;
 };
 
+
+Customer.retrieveAll = function() {
+  return new Promise(function(resolve, reject){
+    sql.query('SELECT * FROM client_users', function(err, rows) {
+      if (err) throw err;
+        const customers = [];
+        rows.forEach((row) => {
+          customers.push(new Customer(row));
+        })
+        resolve(customers);
+    })
+  })
+}
 
 module.exports = Customer;
