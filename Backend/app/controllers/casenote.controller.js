50303b3a310cbdd43cbbe7c9ba1e997757fe9ba7
youssefsoli <youssefdevelops@gmail.com>
2021-11-16 22:36:45 -0500

    Fixed controller logic

--- Backend/app/controllers/casenote.controller.js
+++ Backend/app/controllers/casenote.controller.js
@@ -1,18 +1,24 @@
-const CaseNote = require('../models/casenote.model')
+const CaseNote = require('../models/casenote.model');
 
 const caseNoteController = {
-    create(req, res) {
-        const note = CaseNote(req.body); // TODO: Add data sanitization
-        note.create()
+  create(req, res) {
+    if (req.body) {
+      const note = new CaseNote(req.body); // TODO: Add data sanitization
+      note
+        .create()
         .then(case_id => {
-            res.json(case_id);
+          res.json(case_id);
         })
         .catch(err => {
-            // TODO: Add error handling middleware
-            res.status(500);
-            res.render('error', { error: err });
-        })
+          // TODO: Add error handling middleware
+          res.status(500);
+          res.send({error: err });
+        });
+    } else {
+        res.status(500);
+        res.send({error: 'empty body' });
     }
-}
+  },
+};
 
 module.exports = caseNoteController;
