81a183ce639a0a5739c42e529b81ab07e6166594
youssefsoli <youssefdevelops@gmail.com>
2021-11-16 21:36:30 -0500

    Set last_updated to default to current date

--- schemas/admin_client_schema.sql
+++ schemas/admin_client_schema.sql
@@ -4,31 +4,31 @@ USE homes_for_heroes;
 
 DROP TABLE IF EXISTS admin_users;
 CREATE TABLE IF NOT EXISTS admin_users (
-name VARCHAR(255) NOT NULL UNIQUE,
-email VARCHAR(255) NOT NULL UNIQUE,
-phone VARCHAR(255) NOT NULL UNIQUE,
-password VARCHAR(255) NOT NULL,
-address VARCHAR(255) NOT NULL,
-admin_id int NOT NULL AUTO_INCREMENT,
-primary key (admin_id)
+    name VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    phone VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    admin_id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (admin_id)
 );
 
 DROP TABLE IF EXISTS client_users;
 CREATE TABLE IF NOT EXISTS client_users (
-name VARCHAR(255) NOT NULL UNIQUE,
-email VARCHAR(255) NOT NULL UNIQUE,
-phone VARCHAR(255) NOT NULL UNIQUE,
-password VARCHAR(255) NOT NULL,
-user_id int NOT NULL AUTO_INCREMENT,
-primary key (user_id)
+    name VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    phone VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (user_id)
 );
 
 DROP TABLE IF EXISTS cases;
 CREATE TABLE IF NOT EXISTS cases (
-user_id int NOT NULL ,
-admin_id int NOT NULL ,
-case_id int NOT NULL AUTO_INCREMENT,
-notes TEXT,
-last_update DATETIME,
-primary key (case_id)
+    user_id INT NOT NULL,
+    admin_id INT NOT NULL,
+    notes TEXT,
+    last_update DATETIME NOT NULL DEFAULT NOW(),
+    case_id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (case_id)
 );
