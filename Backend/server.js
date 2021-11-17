e0be550c3b71beb5cc0eb89451155656508d14fd
youssefsoli <youssefdevelops@gmail.com>
2021-11-16 22:06:14 -0500

    Updated to modern server.js w/ cors

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,21 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 
+// TODO: set a fixed origin
+const corsOptions = {
+    origin: "*" // temporarily allow any host for testing
+}
+
+app.use(cors(corsOptions));
+
+
 // parse requests of content-type - application/json
-app.use(bodyParser.json());
+app.use(express.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
