058230f258144d42b243422f60eaf87e7e408c6c
youssefsoli <youssefdevelops@gmail.com>
2021-11-21 03:13:35 -0500

    Switched customer controller to async

--- Backend/app/controllers/customer.controller.js
+++ Backend/app/controllers/customer.controller.js
@@ -4,43 +4,52 @@ const CaseNote = require('../models/casenote.model');
 // Create and Save a new Customer
 
 const customerController = {
-  getAllUsers(req, res) {
-    Customer.retrieveAll()
-      .then(function (results) {
-        res.send({ customers: results });
-      })
-      .catch(function (err) {
-        console.error(err);
-        // TODO error handling
-        res.status(500);
-        res.send({ error: err });
-      });
-  },
-  getAlertCase(req, res) {
-    const case_id = Customer.getAlertCaseId(req.query.user_id);
-    if (case_id !== null) {
-      CaseNote.getById(case_id)
-        .then(caseNote => res.json(caseNote))
-        .catch(err => {
-          console.error(err);
-          // TODO error handling
-          res.status(500);
-          res.send({ error: err });
-        });
-    } else {
-      console.error(err);
-      res.status(500);
-      res.send({ error: 'Case does not exist' });
-    }
-  },
-  setAlertCase(req, res) {
-    const { user_id, case_id } = req.query;
-    Customer.setAlertCaseId(user_id, case_id).catch(err => {
+  async getAllUsers(req, res) {
+    try {
+      const results = await Customer.retrieveAll();
+      res.send({ customers: results });
+    } catch (err) {
       console.error(err);
       // TODO error handling
       res.status(500);
       res.send({ error: err });
-    });
+    }
+  },
+  async getAlertCase(req, res) {
+    try {
+      const case_id = await Customer.getAlertCaseId(req.query.user_id);
+      if (case_id !== null) {
+        CaseNote.getById(case_id)
+          .then(caseNote => res.json(caseNote))
+          .catch(err => {
+            console.error(err);
+            // TODO error handling
+            res.status(500);
+            res.send({ error: err });
+          });
+      } else {
+        console.error(err);
+        res.status(500);
+        res.send({ error: 'Case does not exist' });
+      }
+    } catch (err) {
+      console.error(err);
+      // TODO error handling
+      res.status(500);
+      res.send({ error: err });
+    }
+  },
+  async setAlertCase(req, res) {
+    try {
+      const { user_id, case_id } = req.query;
+      await Customer.setAlertCaseId(user_id, case_id);
+      res.send({ success: true });
+    } catch (err) {
+      console.error(err);
+      // TODO error handling
+      res.status(500);
+      res.send({ error: err });
+    }
   },
 };
 
