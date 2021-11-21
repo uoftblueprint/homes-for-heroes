82ae650baadbe0ca0d28acff941de73a0c638edf
youssefsoli <youssefdevelops@gmail.com>
2021-11-21 03:13:55 -0500

    Switched casenote controller to async

--- Backend/app/controllers/casenote.controller.js
+++ Backend/app/controllers/casenote.controller.js
@@ -2,32 +2,23 @@ const CaseNote = require('../models/casenote.model');
 const Customer = require('../models/customer.model');
 
 const caseNoteController = {
-  create(req, res) {
-    if (req.body) {
-      const note = new CaseNote(req.body); // TODO: Add data sanitization
-      note
-        .create()
-        .then(case_id => {
-          // Check if user doesn't have an alert case_id set
-          if (Customer.getAlertCaseId(req.body.user_id) === null) {
-            Customer.setAlertCaseId(req.body.user_id, case_id)
-              .then(() => {
-                res.json(case_id);
-              })
-              .catch(err => {
-                res.status(500);
-                res.send({ error: err });
-              });
-          } else res.json(case_id);
-        })
-        .catch(err => {
-          // TODO: Add error handling middleware
-          res.status(500);
-          res.send({ error: err });
-        });
-    } else {
+   async create(req, res) {
+    try {
+      if (req.body) {
+        const note = new CaseNote(req.body); // TODO: Add data sanitization
+        const case_id = await note.create();
+        const alert_case_id = await Customer.getAlertCaseId(req.body.user_id);
+        if (alert_case_id === null)
+          await Customer.setAlertCaseId(req.body.user_id, case_id)
+        res.json(case_id);
+      } else {
+        res.status(500);
+        res.send({ error: 'empty body' });
+      }
+    } catch (err) {
+      // TODO: Add error handling middleware
       res.status(500);
-      res.send({ error: 'empty body' });
+      res.send({ error: err });
     }
   },
 };
