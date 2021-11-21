525850b60402ac3dbb81c84a8260849fd110e52d
youssefsoli <youssefdevelops@gmail.com>
2021-11-21 01:58:20 -0500

    Added getAlertCaseId to customer

--- Backend/app/models/customer.model.js
+++ Backend/app/models/customer.model.js
@@ -1,25 +1,38 @@
-const sql = require("./db.js");
+const sql = require('./db.js');
 
 // constructor
-const Customer = function(customer) {
+const Customer = function (customer) {
   this.user_id = customer.user_id;
   this.name = customer.name;
   this.email = customer.email;
   this.phone = customer.phone;
+  this.alert_case_id = customer.alert_case_id;
 };
 
-
-Customer.retrieveAll = function() {
-  return new Promise(function(resolve, reject){
-    sql.query('SELECT * FROM client_users', function(err, rows) {
+Customer.retrieveAll = function () {
+  return new Promise(function (resolve, reject) {
+    sql.query('SELECT * FROM client_users', function (err, rows) {
       if (err) reject(err);
-        const customers = [];
-        rows.forEach((row) => {
-          customers.push(new Customer(row));
-        })
-        resolve(customers);
-    })
-  })
-}
+      const customers = [];
+      rows.forEach(row => {
+        customers.push(new Customer(row));
+      });
+      resolve(customers);
+    });
+  });
+};
+
+Customer.getAlertCaseId = function (clientId) {
+  return new Promise(function (resolve, reject) {
+    sql.query(
+      'SELECT alert_case_id FROM client_users WHERE user_id = ?',
+      [clientId],
+      function (err, rows) {
+        if (err) reject(err);
+        resolve(rows[0]);
+      }
+    );
+  });
+};
 
 module.exports = Customer;
