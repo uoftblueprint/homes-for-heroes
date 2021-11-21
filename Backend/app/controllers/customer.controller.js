72c9c1feb32ffc2d54d97233288687046e6409f3
youssefsoli <youssefdevelops@gmail.com>
2021-11-21 02:19:58 -0500

    Added getAlertCase to customer controller

--- Backend/app/controllers/customer.controller.js
+++ Backend/app/controllers/customer.controller.js
@@ -1,19 +1,39 @@
-const Customer = require("../models/customer.model.js");
+const Customer = require('../models/customer.model');
+const CaseNote = require('../models/casenote.model');
 
 // Create and Save a new Customer
 
-const getAllUsers = ((req, res) => {
-    Customer.retrieveAll()
-    .then(function(results) {
-        res.send({'customers': results});
-    })
-    .catch(function(err){
-        console.error(err);
-        // TODO error handling
-        res.send({'error': err});
-    });
-});
 
-module.exports = {
-    getAllUsers
+const customerController = {
+    getAllUsers(req, res) {
+        Customer.retrieveAll()
+          .then(function (results) {
+            res.send({ customers: results });
+          })
+          .catch(function (err) {
+            console.error(err);
+            // TODO error handling
+            res.status(500);
+            res.send({ error: err });
+          });
+    },
+    getAlertCase (req, res) {
+        const case_id = Customer.getAlertCaseId(req.query.user_id);
+        if (case_id >= 0) {
+          CaseNote.getById(case_id)
+            .then(caseNote => res.json(caseNote))
+            .catch(err => {
+              console.error(err);
+              // TODO error handling
+              res.status(500);
+              res.send({ error: err });
+            });
+        } else {
+          console.error(err);
+          res.status(500);
+          res.send({ error: 'Case does not exist' });
+        }
+      }
 }
+
+module.exports = customerController;
