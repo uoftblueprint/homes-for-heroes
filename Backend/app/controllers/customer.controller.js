0359866e14402e19af6ad73f7d42f8cc77923edd
youssefsoli <youssefdevelops@gmail.com>
2021-11-21 02:40:08 -0500

    Added setAlertCase

--- Backend/app/controllers/customer.controller.js
+++ Backend/app/controllers/customer.controller.js
@@ -3,37 +3,45 @@ const CaseNote = require('../models/casenote.model');
 
 // Create and Save a new Customer
 
-
 const customerController = {
-    getAllUsers(req, res) {
-        Customer.retrieveAll()
-          .then(function (results) {
-            res.send({ customers: results });
-          })
-          .catch(function (err) {
-            console.error(err);
-            // TODO error handling
-            res.status(500);
-            res.send({ error: err });
-          });
-    },
-    getAlertCase (req, res) {
-        const case_id = Customer.getAlertCaseId(req.query.user_id);
-        if (case_id >= 0) {
-          CaseNote.getById(case_id)
-            .then(caseNote => res.json(caseNote))
-            .catch(err => {
-              console.error(err);
-              // TODO error handling
-              res.status(500);
-              res.send({ error: err });
-            });
-        } else {
+  getAllUsers(req, res) {
+    Customer.retrieveAll()
+      .then(function (results) {
+        res.send({ customers: results });
+      })
+      .catch(function (err) {
+        console.error(err);
+        // TODO error handling
+        res.status(500);
+        res.send({ error: err });
+      });
+  },
+  getAlertCase(req, res) {
+    const case_id = Customer.getAlertCaseId(req.query.user_id);
+    if (case_id !== null) {
+      CaseNote.getById(case_id)
+        .then(caseNote => res.json(caseNote))
+        .catch(err => {
           console.error(err);
+          // TODO error handling
           res.status(500);
-          res.send({ error: 'Case does not exist' });
-        }
-      }
-}
+          res.send({ error: err });
+        });
+    } else {
+      console.error(err);
+      res.status(500);
+      res.send({ error: 'Case does not exist' });
+    }
+  },
+  setAlertCase(req, res) {
+    const { user_id, case_id } = req.query;
+    Customer.setAlertCaseId(user_id, case_id).catch(err => {
+      console.error(err);
+      // TODO error handling
+      res.status(500);
+      res.send({ error: err });
+    });
+  },
+};
 
 module.exports = customerController;
