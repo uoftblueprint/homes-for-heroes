c228c614044ab61f64cd6d3c687a4808fbfacc5f
youssefsoli <youssefdevelops@gmail.com>
2021-11-28 19:05:27 -0500

    Added SQL join for fetching alert case

--- Backend/app/models/customer.model.js
+++ Backend/app/models/customer.model.js
@@ -38,6 +38,20 @@ Customer.getAlertCaseId = function (user_id) {
   });
 };
 
+Customer.getAlertCase = function (user_id) {
+  return new Promise(function (resolve, reject) {
+    sql.query(
+      'SELECT cases.* FROM client_users INNER JOIN cases ON client_users.alert_case_id = cases.case_id WHERE client_users.user_id = ?'
+      [user_id],
+      function (err, rows) {
+        if (err) reject(err);
+        else if (rows[0] === undefined) reject('Alert note does not exist.');
+        else resolve(rows[0]);
+      }
+    );
+  });
+};
+
 Customer.setAlertCaseId = function (user_id, case_id) {
   return new Promise(function (resolve, reject) {
     sql.query(
