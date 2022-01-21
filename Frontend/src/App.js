c2cae6b6dd6527733e40f565745d4c0d59a94466
Jeffery Zhan <jefferyzhan84@gmail.com>
2022-01-21 18:22:39 -0500

    removed home component

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -3,27 +3,23 @@ import './App.css';
 
 import NavBar from './components/NavBar';
 import { Route, Switch } from "react-router-dom";
+import { Typography } from "@mui/material/"
 
 import ProfilePage from './components/ProfilePage.js';
 import CRM from "./components/CRM"
 import CaseList from "./components/CaseList"
 import Login from "./components/Login"
-import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Switch>
-        <Route exact from="/" render={props => <Home {...props} />} />
+        <Route exact from="/" render={props => <Typography color='black'>Home</Typography>} />
         <Route exact path="/usercrm" render={props => <CRM {...props} />} />
         <Route exact path="/usercase" render={props => <CaseList {...props} />} />
         <Route exact path="/profilepage" render={props => <ProfilePage {...props} />} />
         <Route exact path="/login" render={props => <Login {...props} />} />
-        {/* <Route path="/Home" component={Home} />
-        <Route path="/CaseDetails" component={CaseDetails} />
-        <Route path="/UserList" component={UserList} />
-        <Route path="Login" component={Login} /> */}
       </Switch>
     </div>
   );
