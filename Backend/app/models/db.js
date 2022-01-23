badf6f2abf66f982be856b7d76e0b1302b148933
yrao6597 <yvonne.yrao@gmail.com>
2022-01-23 11:56:22 -0500

    modify db.js to support processing multiple queries

--- Backend/app/models/db.js
+++ Backend/app/models/db.js
@@ -5,7 +5,8 @@ var connection = mysql.createPool({
   host: dbConfig.HOST,
   user: dbConfig.USER,
   password: dbConfig.PASSWORD,
-  database: dbConfig.DB
+  database: dbConfig.DB,
+  multipleStatements: true
 });
 
 module.exports = connection;
