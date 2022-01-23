7a1727572419b19fe66b332a4ed1e6d23c5138e2
yrao6597 <yvonne.yrao@gmail.com>
2022-01-23 13:45:53 -0500

    add routes

--- Backend/server.js
+++ Backend/server.js
@@ -24,6 +24,7 @@ app.get("/", (req, res) => {
 
 require("./app/routes/customer.routes")(app);
 require("./app/routes/casenote.routes")(app);
+require("./app/routes/admin.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 3000;
