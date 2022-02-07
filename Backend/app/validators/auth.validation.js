98ac06568490fef4e5b3be3d07009afd0aa7d67e
youssefsoli <youssefdevelops@gmail.com>
2022-02-06 21:03:06 -0500

    Added validation schemas

---
+++ Backend/app/validators/auth.validation.js
@@ -0,0 +1,16 @@
+const { body } = require('express-validator');
+
+const validationSchema = {
+  signUpSchema: [
+    body('name').trim().notEmpty().escape(),
+    body('email').isEmail().normalizeEmail(),
+    body('phone').isMobilePhone(),
+    body('password').isStrongPassword()
+  ],
+  loginSchema: [
+    body('email').notEmpty(),
+    body('password').notEmpty()
+  ]
+}
+
+module.exports = validationSchema;
