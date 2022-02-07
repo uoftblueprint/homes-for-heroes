87ee5dc4f1b18fc0ad64bdf9c5349bbd4586bc7c
youssefsoli <youssefdevelops@gmail.com>
2022-02-07 02:34:07 -0500

    Fixed logger typo

--- Backend/app/middleware/catch-all-error-handler.js
+++ Backend/app/middleware/catch-all-error-handler.js
@@ -3,7 +3,7 @@ const logger = require('../logger');
 // eslint-disable-next-line no-unused-vars
 const catchAllErrorHandler = (error, req, res, next) => {
   res.status(500).send(error);
-  logger.error(`${res.status} ${req.method} ${req.url} ${error}`);
+  logger.error(`${res.statusCode} ${req.method} ${req.url} ${error}`);
 };
 
 module.exports = catchAllErrorHandler;
