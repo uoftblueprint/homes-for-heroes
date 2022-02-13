3e88d16b250e26fcf085f0623bdeb7105710dcc5
youssefsoli <youssefdevelops@gmail.com>
2022-02-13 11:50:48 -0500

    Switched ratelimiter to the api endpoints

--- Backend/app/routes/index.js
+++ Backend/app/routes/index.js
@@ -1,6 +1,9 @@
 const { Router } = require('express');
 const apiRouter = Router();
+const rateLimiter = require('../middleware/ratelimiter');
 
+// Rate limit every api endpoint
+apiRouter.use(rateLimiter);
 require('./customer.routes')(apiRouter);
 require('./casenote.routes')(apiRouter);
 require('./auth.routes')(apiRouter);
