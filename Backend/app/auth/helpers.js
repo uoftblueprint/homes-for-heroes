dcf88d9f46905fc6013dfcc26bb8f40282711ec8
youssefsoli <youssefdevelops@gmail.com>
2022-02-13 15:56:10 -0500

    Added authentication helpers

---
+++ Backend/app/auth/helpers.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+
+const helpers = {
+  issueUserJWT(user) {
+    const payload = {
+      id: user.user_id,
+      role: 1, // TODO: Change role based on type of user (client: 1, admin: 2, superadmin: 3)
+    };
+
+    return jwt.sign(payload, 'CHANGEME', { expiresIn: '1d' });
+  },
+  verifyUserJWT(token) {
+    return jwt.verify(token, 'CHANGEME');
+  },
+  issueEmailJWT(user) {
+    const payload = {
+      id: user.user_id
+    };
+
+    return jwt.sign(payload, 'EMAIL_CHANGEME', { expiresIn: '1d' });
+  },
+  verifyEmailJWT(token) {
+    return jwt.verify(token, 'EMAIL_CHANGEME');
+  },
+};
+
+module.exports = helpers;
