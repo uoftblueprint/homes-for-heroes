f341d38df09ae0dc7dd761860434139657059911
youssefsoli <youssefdevelops@gmail.com>
2022-02-13 11:50:22 -0500

    Added pm2 config to enable clustering

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+// pm2 config file
+
+module.exports = {
+  apps: [
+    {
+      name: 'homes-for-heroes-app',
+      script: 'Backend/server.js',
+      instances: -1, // Spread app across all cpus except one
+      exec_mode: 'cluster', // The app will be clustered
+      env: {
+        'NODE_ENV': 'production'
+      }
+    },
+  ],
+};
