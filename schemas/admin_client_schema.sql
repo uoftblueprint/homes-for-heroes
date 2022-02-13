62aaedc635dd1b43d4ad65821ea213b17572215c
youssefsoli <youssefdevelops@gmail.com>
2022-02-13 08:54:20 -0500

    Added verification to schema

--- schemas/admin_client_schema.sql
+++ schemas/admin_client_schema.sql
@@ -21,9 +21,11 @@ CREATE TABLE IF NOT EXISTS client_users (
     email VARCHAR(255) NOT NULL UNIQUE,
     phone VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
+    verified BOOLEAN NOT NULL,
+    verification_code VARCHAR(32) UNIQUE,
     alert_case_id INT UNIQUE,
     user_id INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (user_id)
+    PRIMARY KEY (user_id),
     FOREIGN KEY (alert_case_id)
     REFERENCES cases(case_id)
         ON UPDATE CASCADE
@@ -37,7 +39,7 @@ CREATE TABLE IF NOT EXISTS cases (
     notes TEXT,
     last_update DATETIME NOT NULL DEFAULT NOW(),
     case_id INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (case_id)
+    PRIMARY KEY (case_id),
     FOREIGN KEY (user_id)
 	REFERENCES client_users (user_id)
         ON UPDATE CASCADE
