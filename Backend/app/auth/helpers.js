f71da5d7a81f425dc2ba9990663e1a12fc2e9543
youssefsoli <youssefdevelops@gmail.com>
2022-02-13 16:42:19 -0500

    Switched JWT secret to environment variable

--- Backend/app/auth/helpers.js
+++ Backend/app/auth/helpers.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
 const helpers = {
@@ -7,20 +8,20 @@ const helpers = {
       role: 1, // TODO: Change role based on type of user (client: 1, admin: 2, superadmin: 3)
     };
 
-    return jwt.sign(payload, 'CHANGEME', { expiresIn: '1d' });
+    return jwt.sign(payload, process.env.JWT_USER_SECRET, { expiresIn: '1d' });
   },
   verifyUserJWT(token) {
-    return jwt.verify(token, 'CHANGEME');
+    return jwt.verify(token, process.env.JWT_USER_SECRET);
   },
   issueEmailJWT(user) {
     const payload = {
       id: user.user_id,
     };
 
-    return jwt.sign(payload, 'EMAIL_CHANGEME', { expiresIn: '1d' });
+    return jwt.sign(payload, process.env.JWT_EMAIL_SECRET, { expiresIn: '1d' });
   },
   verifyEmailJWT(token) {
-    return jwt.verify(token, 'EMAIL_CHANGEME');
+    return jwt.verify(token, process.env.JWT_EMAIL_SECRET);
   },
 };
 
