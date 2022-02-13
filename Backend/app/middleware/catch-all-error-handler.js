4f03121f4d951782c17230dfb3d54713b221b90c
youssefsoli <youssefdevelops@gmail.com>
2022-02-13 16:36:16 -0500

    Fixed error handler bug

--- Backend/app/middleware/catch-all-error-handler.js
+++ Backend/app/middleware/catch-all-error-handler.js
@@ -2,7 +2,7 @@ const logger = require('../logger');
 
 // eslint-disable-next-line no-unused-vars
 const catchAllErrorHandler = (error, req, res, next) => {
-  res.status(500).send(error);
+  res.status(500).json({ error: error.message ? error.message : error });
   logger.error('%s %s %s %s', res.statusCode, req.method, req.url, error);
 };
 
