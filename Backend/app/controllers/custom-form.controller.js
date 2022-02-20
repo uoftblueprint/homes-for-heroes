dd7e03dca02ceddfd461bc2e75588b52179b1f78
EclipseIsDead <sidmcheath@gmail.com>
2022-02-20 16:08:19 -0500

    fixed main routes

--- Backend/app/controllers/custom-form.controller.js
+++ Backend/app/controllers/custom-form.controller.js
@@ -6,8 +6,25 @@ const customFormController = {
     async getCustomForm(req, res) {
         try {
             const { form_id } = req.params;
-            const resForm = await CustomForm.queryForm({form_id: form_id});
+            console.log(`get custom form id ${form_id}`)
+            const resForm = await CustomForm.queryForm({ form_id: form_id });
             res.send(resForm);
+        } catch (err) {
+            console.error(err);
+            res.status(500);
+            res.send({ error: err });
+        }
+    },
+
+    async updateCustomForm(req, res) {
+        try {
+            const { form_id } = req.params;
+            console.log("update controller")
+            console.log(req.body)
+            console.log({ form_id: form_id, ...req.body })
+            const form = new CustomForm({ form_id: form_id, ...req.body });
+            await form.update();
+            res.status(200);
         } catch (err) {
             console.error(err);
             res.status(500);
@@ -17,7 +34,6 @@ const customFormController = {
 
     async createCustomForm(req, res) {
         try {
-            console.log(req.body);
             const form = new CustomForm(req.body);
             const form_id = await form.create();
             res.json(form_id);
@@ -28,10 +44,30 @@ const customFormController = {
         }
     },
 
-    async queryCustomForms(req, res) {
+    async queryAllAdminForms(req, res) {
         try {
-            const resForm = await CustomForm.queryForm(req.query);
-            res.send(resForm);
+            const { admin_id } = req.params;
+            const [completedForms, drafts] = await Promise.all([
+                await CustomForm.queryForm({ admin_id: admin_id, is_final: true }),
+                await CustomForm.queryForm({ admin_id: admin_id, is_final: false })
+            ]);
+            res.send({
+                'completed': completedForms,
+                'drafts': drafts
+            });
+        } catch (err) {
+            console.error(err);
+            res.status(500);
+            res.send({ error: err });
+        }
+    },
+
+    async publishForm(req, res) {
+        try {
+            const { form_id } = req.params;
+            const form = new CustomForm(req.body);
+            await form.publish({ form_id: form_id });
+            res.status(200);
         } catch (err) {
             console.error(err);
             res.status(500);
