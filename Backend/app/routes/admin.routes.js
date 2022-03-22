6c0360ba90a608ef8b221a40c4ca1c5c02a52db7
youssefsoli <youssefdevelops@gmail.com>
2022-03-22 16:09:36 -0400

    Fixed spelling error

--- Backend/app/routes/admin.routes.js
+++ Backend/app/routes/admin.routes.js
@@ -1,32 +1,31 @@
-const admin = require("../controllers/admin.controller");
+const admin = require('../controllers/admin.controller');
 const validationSchema = require('../validators/admin.validation');
 const validationErrorHandler = require('../middleware/validation-error-handler');
-const passport = require('passport');
 
 module.exports = app => {
-    app.get('/admins/getAll', admin.listAll);
+  app.get('/admins/getAll', admin.listAll);
 
-    app.put(
-        '/admins/:admin_id/makeSuperadmin', 
-        validationSchema.makeSuperadminSchema,
-        validationErrorHandler,
-        admin.makeSuperadmin);
+  app.put(
+    '/admins/:admin_id/makeSuperadmin', 
+    validationSchema.makeSuperadminSchema,
+    validationErrorHandler,
+    admin.makeSuperadmin);
 
-    app.put(
-        '/admins/:admin_id/unsetSuperadmin', 
-        validationSchema.unsetSuperadminSchema,
-        validationErrorHandler,
-        admin.unsetSuperadmin);
+  app.put(
+    '/admins/:admin_id/unsetSuperadmin', 
+    validationSchema.unsetSuperadminSchema,
+    validationErrorHandler,
+    admin.unsetSuperadmin);
 
-    app.put(
-        '/admins/:admin_id/assignChapter', 
-        validationSchema.assignChapter,
-        validationErrorHandler,
-        admin.assignChapter);
+  app.put(
+    '/admins/:admin_id/assignChapter', 
+    validationSchema.assignChapterSchema,
+    validationErrorHandler,
+    admin.assignChapter);
 
-    app.get(
-        '/admins/:chapter/listByChapter', 
-        validationSchema.listChapterSupervisorsSchema,
-        validationErrorHandler,
-        admin.getByChapter);
+  app.get(
+    '/admins/:chapter/listByChapter', 
+    validationSchema.listChapterSupervisorsSchema,
+    validationErrorHandler,
+    admin.getByChapter);
 };
