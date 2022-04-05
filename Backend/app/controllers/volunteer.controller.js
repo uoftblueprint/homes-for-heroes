056fe40f249fd7240addfcea2f137b865f143401
youssefsoli <youssefdevelops@gmail.com>
2022-04-04 20:59:54 -0400

    Switched to using error middleware

--- Backend/app/controllers/volunteer.controller.js
+++ Backend/app/controllers/volunteer.controller.js
@@ -2,30 +2,26 @@ const Volunteer = require('../models/volunteer.model');
 const logger = require('../logger');
 
 const volunteerController = {
-    async getAllVolunteers(req, res) {
-        try {
-            const results = await Volunteer.listAll();
-            res.send({ volunteers: results });
-        } catch (err) {
-            console.error(err);
-            res.status(500);
-            res.send({ error: err });
-        }
-    },
-
-    async create(req, res) {
-        try {
-            logger.debug(req.body);
-            const new_volunteer = new Volunteer(req.body);
-            const volunteer_id = await new_volunteer.create();
-            logger.debug(volunteer_id);
-            res.json(volunteer_id);
-        } catch (err) {
-            console.error(err);
-            res.status(500);
-            res.send({ error: err });
-        }
+  async getAllVolunteers(req, res, next) {
+    try {
+      const results = await Volunteer.listAll();
+      res.send({ volunteers: results });
+    } catch (err) {
+      next(err);
     }
+  },
+
+  async create(req, res, next) {
+    try {
+      logger.debug(req.body);
+      const new_volunteer = new Volunteer(req.body);
+      const volunteer_id = await new_volunteer.create();
+      logger.debug(volunteer_id);
+      res.json(volunteer_id);
+    } catch (err) {
+      next(err);
+    }
+  }
 };
 
 module.exports = volunteerController;
