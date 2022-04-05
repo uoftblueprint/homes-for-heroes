0b63305f25455bae79f422988ed0d8cc6745bb25
youssefsoli <youssefdevelops@gmail.com>
2022-04-04 21:02:52 -0400

    Switched to new admin-client schema design

--- Backend/app/models/admin.model.js
+++ Backend/app/models/admin.model.js
@@ -1,56 +1,106 @@
 const sql = require('./db.js');
 const logger = require('../logger');
-const bcrypt = require('bcrypt');
+const Customer = require('./customer.model');
 
 // constructor
-const Admin = function (body) {
-    this.name = body.name;
-    this.role_id = 1; // 0 by default
+const Admin = function (admin) {
+  this.user_id = admin.user_id;
+  this.chapter_id = admin.chapter_id;
+  this.role_id = admin.role_id || 1;
+  this.adminCustomer = admin.adminCustomer;
 };
 
-Admin.create = function (name, email, phone, password, address, chapter_id) {
-    return new Promise((resolve, reject) => {
-      const hashedPassword = bcrypt.hashSync(password, 15);
-      sql.query(
-        'INSERT INTO admin_users (name, email, phone, password, address, chapter_id, role_id) VALUES (?, ?, ?, ?, ?, ?, 1)',
-        [name, email, phone, hashedPassword, address, chapter_id],
-        (err) => {
-          if (err) reject(err);
-          else {
-            sql.query('SELECT LAST_INSERT_ID() as admin_id', (err, rows) => {
-              if (err) reject(err);
-              else {
-                // eslint-disable-next-line prefer-destructuring
-                const [ user_id ] = rows;
-                resolve(
-                  new Admin({
-                    name: name,
-                    role_id: 1,
-                  }),
-                );
-              }
+Admin.create = function (name, email, phone, password, chapter_id, role_id = 1) {
+  return new Promise((resolve, reject) => {
+    sql.getConnection((err, conn) => {
+      if (err) return reject(err);
+      conn.beginTransaction((err) => {
+        if (err) return reject(err);
+        const adminCustomer = Customer.create(name, phone, email, password, role_id, conn); // role_id of 1 (supervisor) by default
+        conn.query(
+          'INSERT INTO admin_users (user_id, chapter_id) VALUES (?, ?)',
+          [adminCustomer.user_id, chapter_id],
+          (err, result) => {
+            if (err) return conn.rollback(() => reject(err));
+            else if (!result.insertId) return reject(new Error('Could not create admin'));
+
+            conn.commit((err) => {
+              if (err) return conn.rollback(() => reject(err));
+              // Return a new admin
+              resolve(new Admin({
+                user_id: adminCustomer.user_id,
+                chapter_id,
+                role_id,
+                adminCustomer
+              }));
             });
-          }
-        },
-      );
+          });
+      });
     });
-  };
+  });
+};
+
+Admin.createTemp = function (name, email, chapter_id, role_id = 1) {
+  return new Promise((resolve, reject) => {
+    sql.getConnection((err, conn) => {
+      if (err) return reject(err);
+
+      conn.beginTransaction((err) => {
+        if (err) return reject(err);
+        const adminCustomer = Customer.createTemp(name, email, role_id, conn); // role_id of 1 (supervisor) by default
+        conn.query(
+          'INSERT INTO admin_users (user_id, chapter_id) VALUES (?, ?)',
+          [adminCustomer.user_id, chapter_id],
+          (err, result) => {
+            if (err) return conn.rollback(() => reject(err));
+            else if (!result.insertId) return reject(new Error('Could not create admin'));
+
+            conn.commit((err) => {
+              if (err) return conn.rollback(() => reject(err));
+              // Return a new admin
+              resolve(new Admin({
+                user_id: adminCustomer.user_id,
+                chapter_id,
+                role_id,
+                adminCustomer
+              }));
+            });
+          });
+      });
+    });
+  });
+};
 
 Admin.listAll = function () {
-    return new Promise(function (resolve, reject) {
-        sql.query('SELECT * FROM admin_users', function (err, admins) {
-            if (err) reject(err);
-            else {
-                resolve(admins);
-            }
-        });
+  return new Promise((resolve, reject) => {
+    sql.query('SELECT a.*, c.* FROM admin_users AS a JOIN client_users AS c ON a.user_id = c.user_id', (err, admins) => {
+      if (err) reject(err);
+      else {
+        logger.debug('Admin.listAll: %o', admins);
+        resolve(admins);
+      }
     });
+  });
+};
+
+Admin.listAllRole = function (role_id) {
+  return new Promise((resolve, reject) => {
+    sql.query('SELECT a.*, c.* FROM admin_users AS a JOIN client_users AS c ON a.user_id = c.user_id WHERE c.role_id = ?',
+      [role_id],
+      (err, admins) => {
+        if (err) reject(err);
+        else {
+          logger.debug('Admin.listAllRole: %o, %d', admins, role_id);
+          resolve(admins);
+        }
+      });
+  });
 };
 
 Admin.getSearchAdmins = function (name) {
   return new Promise((resolve, reject) => {
     sql.query(
-      'SELECT * FROM admin_users WHERE name LIKE ?',
+      'SELECT a.*, c.* FROM admin_users AS a JOIN client_users AS c ON a.user_id = c.user_id WHERE c.name LIKE ? AND c.role_id > 0',
       [`%${name}%`],
       (err, admins) => {
         if (err) reject(err);
@@ -61,31 +111,31 @@ Admin.getSearchAdmins = function (name) {
 };
 
 Admin.makeSupervisor = function(admin_id) {
-    return new Promise(function (resolve, reject) {
-        sql.query('UPDATE admin_users SET role_id = 1 WHERE admin_id = ?',
-        [admin_id],
-        function (err, rows) {
-            if (err) reject (err);
-            else resolve(rows[0]);
-        });
-    });
+  return new Promise((resolve, reject) => {
+    sql.query('UPDATE client_users SET role_id = 1 WHERE user_id = ?',
+      [admin_id],
+      (err, rows) => {
+        if (err) reject (err);
+        else resolve(rows[0]);
+      });
+  });
 };
 
 Admin.makeSuperadmin = function(admin_id) {
-    return new Promise(function (resolve, reject) {
-        sql.query('UPDATE admin_users SET role_id = 2 WHERE admin_id = ?',
-        [admin_id],
-        function (err, rows) {
-            if (err) reject (err);
-            else resolve(rows[0]);
-        });
-    });
+  return new Promise((resolve, reject) => {
+    sql.query('UPDATE client_users SET role_id = 2 WHERE user_id = ?',
+      [admin_id],
+      (err, rows) => {
+        if (err) reject (err);
+        else resolve(rows[0]);
+      });
+  });
 };
 
 Admin.unsetSupervisor = function (admin_id) {
   return new Promise((resolve, reject) => {
     sql.query(
-      'UPDATE admin_users SET role_id = 1 WHERE admin_id = ?',
+      'UPDATE client_users SET role_id = 1 WHERE user_id = ?',
       [admin_id],
       (err, rows) => {
         if (err) reject(err);
@@ -96,21 +146,21 @@ Admin.unsetSupervisor = function (admin_id) {
 };
 
 Admin.unsetSuperadmin = function(admin_id) {
-    return new Promise(function (resolve, reject) {
-        sql.query('UPDATE admin_users SET role_id = 1 WHERE admin_id = ?',
-        [admin_id],
-        function (err, rows) {
-            if (err) reject (err);
-            else resolve(rows[0]);
-        });
-    });
-}
+  return new Promise((resolve, reject) => {
+    sql.query('UPDATE client_users SET role_id = 1 WHERE user_id = ?',
+      [admin_id],
+      (err, rows) => {
+        if (err) reject (err);
+        else resolve(rows[0]);
+      });
+  });
+};
 
 Admin.assignChapter = function (admin_id, chapter_id) {
   logger.debug(admin_id);
   return new Promise((resolve, reject) => {
     sql.query(
-      'UPDATE admin_users SET chapter_id = ? WHERE admin_id = ?',
+      'UPDATE admin_users SET chapter_id = ? WHERE user_id = ?',
       [chapter_id, admin_id],
       (err) => {
         if (err) reject(err);
@@ -123,15 +173,16 @@ Admin.assignChapter = function (admin_id, chapter_id) {
 };
 
 Admin.listByChapter = function(chapter_id) {
-    return new Promise(function (resolve, reject) {
-        sql.query('SELECT * FROM admin_users where role_id = 1 and chapter_id = ?', [chapter_id], function (err, supervisors) {
-            if (err) reject (err);
-            else {
-                resolve(supervisors);
-            }
-        });
-    });
-}
+  return new Promise((resolve, reject) => {
+    sql.query('SELECT a.*, c.* FROM admin_users AS a JOIN client_users AS c ON a.user_id = c.user_id WHERE c.role_id = 1 AND a.chapter_id = ?',
+      [chapter_id], (err, supervisors) => {
+        if (err) reject (err);
+        else {
+          resolve(supervisors);
+        }
+      });
+  });
+};
 
 
 
