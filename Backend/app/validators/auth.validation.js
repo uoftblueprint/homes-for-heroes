86590ba629ebd3fb5a00d6deef5543704f1aec95
youssefsoli <youssefdevelops@gmail.com>
2022-04-04 21:07:59 -0400

    Added new createAdmin route and refactored createVeteran route

--- Backend/app/validators/auth.validation.js
+++ Backend/app/validators/auth.validation.js
@@ -11,6 +11,11 @@ const validationSchema = {
     body('name').trim().notEmpty().escape(),
     body('email').isEmail().normalizeEmail(),
   ],
+  createAdminSchema: [
+    body('name').trim().notEmpty().escape(),
+    body('email').isEmail().normalizeEmail(),
+    body('chapter_id').isInt({ min: 0 }),
+  ],
 
   loginSchema: [body('email').notEmpty(), body('password').notEmpty()],
   verifySchema: [param('verificationCode').isJWT()],
