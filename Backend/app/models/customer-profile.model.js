7436e1cc8481b1077c787065a79bd473a0c70137
youssefsoli <youssefdevelops@gmail.com>
2022-04-04 21:05:36 -0400

    Reformatted file

--- Backend/app/models/customer-profile.model.js
+++ Backend/app/models/customer-profile.model.js
@@ -1,6 +1,3 @@
-const Customer = require("./customer.model");
-const { query } = require("./db");
-
 const CustomerProfile = function(user_id, body) {
   this.user_id = user_id;
   this.name = body.name;
@@ -10,23 +7,21 @@ const CustomerProfile = function(user_id, body) {
   this.city = body.city;
   this.province = body.province;
   this.applicant_dob = body.applicant_dob;
-}
+};
 
 CustomerProfile.prototype.buildCQuery = function() {
-  var update_qry = `UPDATE client_users SET name = '${this.name}', email = '${this.email}', phone = '${this.phone}' WHERE user_id = ${this.user_id}`;
-  return update_qry;
-}
+  return `UPDATE client_users SET name = '${this.name}', email = '${this.email}', phone = '${this.phone}' WHERE user_id = ${this.user_id}`;
+};
 
 CustomerProfile.prototype.buildUQuery = function() {
-  var update_qry = `UPDATE UserInfo SET street_name = '${this.street_name}', city = '${this.city}', province = '${this.province}', applicant_dob = '${this.applicant_dob}' WHERE user_id = ${this.user_id}`;
-  return update_qry;
-}
+  return `UPDATE UserInfo SET street_name = '${this.street_name}', city = '${this.city}', province = '${this.province}', applicant_dob = '${this.applicant_dob}' WHERE user_id = ${this.user_id}`;
+};
 
 CustomerProfile.prototype.buildQueries = function() {
-  var queries = [];
+  const queries = [];
   queries.push(this.buildCQuery());
   queries.push(this.buildUQuery());
   return queries;
-}
+};
 
 module.exports = CustomerProfile;
