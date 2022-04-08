e702c14524a7609da5730df3a827651dad88bfa5
yrao6597 <yvonne.yrao@gmail.com>
2022-04-08 00:09:39 -0400

    fix formatting

--- Backend/app/validators/supporter.validation.js
+++ Backend/app/validators/supporter.validation.js
@@ -1,14 +1,14 @@
 const { body, query, param } = require('express-validator');
 
 const validationSchema = {
-    createSupporterSchema: [
-        body('name').trim().notEmpty(),
-        body('date_gifted').isDate({ format: 'YYYY-MM-DD' }).withMessage('date_gifted must be in format YYYY-MM-DD'),
-        body('gift_provided').trim().notEmpty(),
-    ],
-    getSupporterSchema: [
-        param('name').trim().notEmpty(),
-    ],
+  createSupporterSchema: [
+    body('name').trim().notEmpty(),
+    body('date_gifted')
+      .isDate({ format: 'YYYY-MM-DD' })
+      .withMessage('date_gifted must be in format YYYY-MM-DD'),
+    body('gift_provided').trim().notEmpty(),
+  ],
+  getSupporterSchema: [param('name').trim().notEmpty()],
 };
 
 module.exports = validationSchema;
--- Backend/app/validators/volunteer.validation.js
+++ Backend/app/validators/volunteer.validation.js
@@ -1,13 +1,13 @@
-const { body, query, param } = require('express-validator');
+const { body, param } = require('express-validator');
 
 const validationSchema = {
-    createVolunteerSchema: [
-        body('name').trim().notEmpty(),
-        body('date_joined').isDate({ format: 'YYYY-MM-DD' }).withMessage('date_joined must be in format YYYY-MM-DD'),
-    ],
-    getVolunteerSchema: [
-        param('name').trim().notEmpty(),
-    ],
+  createVolunteerSchema: [
+    body('name').trim().notEmpty(),
+    body('date_joined')
+      .isDate({ format: 'YYYY-MM-DD' })
+      .withMessage('date_joined must be in format YYYY-MM-DD'),
+  ],
+  getVolunteerSchema: [param('name').trim().notEmpty()],
 };
 
 module.exports = validationSchema;
