de3559e48890c40bf8754678f4142f0bf205e848
yrao6597 <70776900+yrao6597@users.noreply.github.com>
2022-04-08 00:30:46 -0400

    fix merge conflict

--- Backend/app/validators/volunteer.validation.js
+++ Backend/app/validators/volunteer.validation.js
@@ -12,19 +12,9 @@ const volunteerId = [
 ];
 
 const validationSchema = {
-<<<<<<< HEAD
-  createVolunteerSchema: [
-    body('name').trim().notEmpty(),
-    body('date_joined')
-      .isDate({ format: 'YYYY-MM-DD' })
-      .withMessage('date_joined must be in format YYYY-MM-DD'),
-  ],
-  getVolunteerSchema: [param('name').trim().notEmpty()],
-=======
   createVolunteerSchema: [...volunteerInfoSchema],
   getVolunteerSchema: [param('name').trim().notEmpty()],
   updateVolunteerSchema: [...volunteerInfoSchema, ...volunteerId],
->>>>>>> a4692745cc6c1fcb8afda55fa539e7fe80ed5916
 };
 
 module.exports = validationSchema;
