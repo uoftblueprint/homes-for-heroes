8a9d213f39c750e1410ca0cd9359a3ae1159cfcd
yrao6597 <yvonne.yrao@gmail.com>
2022-04-07 23:26:29 -0400

    modify code to use new error middleware

--- Backend/app/controllers/admin.controller.js
+++ Backend/app/controllers/admin.controller.js
@@ -2,104 +2,91 @@ const Admin = require('../models/admin.model');
 const logger = require('../logger');
 
 const adminController = {
-    async createAdmin(req, res) {
-        const { name, email, phone, password, address, chapter_id } = req.body; 
+    async createAdmin(req, res, next) {
+        const { name, email, phone, password, chapter_id } = req.body; 
         try {
-            const admin = await Admin.create(name, email, phone, password, address, chapter_id);
-            res.json({ success: true });
+            const admin = await Admin.create(name, email, phone, password, chapter_id);
+            res.json(admin);
         } catch (err) {
-            console.error(err);
-            res.status(500);
-            res.send({ error: err });
+            next(err);
         }
     },  
-    async listAll(req, res) {
+    async listAll(req, res, next) {
         try {
             const admins = await Admin.listAll();
             res.send({ admins: admins });
         } catch (err) {
-            console.error(err);
-            res.status(500);
-            res.send({ error: err });
+            next(err);
         }
     },
     
-    async getSearchAdmins(req, res) {
+    async getSearchAdmins(req, res, next) {
       try {
         const { name } = req.query;
         const admins = await Admin.getSearchAdmins(name);
         res.send({ admins: admins });
       } catch (err) {
-        console.error(err);
-        res.status(500);
-        res.send({ error: err })
+        next(err);
       }
     },
 
     
-    async makeSuperadmin(req, res) {
+    async makeSuperadmin(req, res, next) {
         try {
             const { admin_id } = req.params;
             const role_id = await Admin.getRole(admin_id);
-            logger.debug(role_id);
             if (role_id == 1) {
-                const results = await Admin.makeSuperadmin(admin_id); 
-                res.send(results);
+                await Admin.makeSuperadmin(admin_id); 
+                res.send({ success: true });
             } else {
-                res.send({ error: "cannot set as superadmin" });
+              next(new Error('Not an admin, cannot set as superadmin.'));
             }
         } catch (err) {
-            console.error(err);
-            res.status(500);
-            res.send({ error: err });
+            next(err);
         }
     },
 
-    async unsetSuperadmin(req, res) {
+    async unsetSuperadmin(req, res, next) {
         try {
             const { admin_id } = req.params;
             const role_id = await Admin.getRole(admin_id);
-            logger.debug(role_id);
-            if (role_id == 1) {
-                const results = await Admin.unsetSuperadmin(admin_id);
-                res.send(results);
+            if (role_id == 2) {
+                await Admin.unsetSuperadmin(admin_id);
+                res.send({ success: true });
             } else {
-                res.send({ error: "not a superadmin; cannot unset superadmin status" });
+              next(new Error('Not a superadmin, cannot unset superadmin status.'));
             }
         } catch (err) {
-            console.error(err);
-            res.status(500);
-            res.send({ error: err });
+            next(err);
         }
     },
 
 
-    async getByChapter(req, res) {
+    async listByChapter(req, res, next) {
         try {
             const { chapter } = req.params;
             const chapter_id = await Chapter.getId(chapter);
-            logger.debug(chapter_id);
-            const results = await Supervisor.listByChapter(chapter_id);
-            res.send(results);
+            const chapterAdmins = await Admin.listByChapter(chapter_id);
+            res.send({ admins: chapterAdmins });
         } catch (err) {
-            console.error(err);
-            res.status(500);
-            res.send({ error: err });
+            next(err);
         }
     },
 
-    async assignChapter(req, res) {
+    async assignChapter(req, res, next) {
         try {
             const chapter_name = req.body.name;
             const { admin_id } = req.params;
             const chapter_id = await Chapter.getId(chapter_name);
-            logger.debug(chapter_id);
-            const results = await Supervisor.assignChapter(admin_id, chapter_id);
-            res.send(results);
+            const role_id = await Admin.getRole(admin_id);
+            if (role_id == 1) {
+              await Admin.assignChapter(admin_id, chapter_id);
+              res.send({ success: true });
+            } else {
+              next(new Error('Can only assign chapters to admins'));
+            }
         } catch (err) {
-            console.error(err);
-            res.status(500);
-            res.send({ error: err });
+            next(err);
         }
     }
 }
