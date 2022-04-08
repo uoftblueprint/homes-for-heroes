1163ec895f2b658b36ac4a5148e69e1a6446c25a
yrao6597 <yvonne.yrao@gmail.com>
2022-04-08 00:03:50 -0400

    add validators

--- Backend/app/routes/chapter.routes.js
+++ Backend/app/routes/chapter.routes.js
@@ -1,7 +1,6 @@
 const chapters = require("../controllers/chapter.controller");
 const validationSchema = require("../validators/chapter.validation");
 const validationErrorHandler = require('../middleware/validation-error-handler');
-const passport = require('passport');
 
 module.exports = app => {
     app.get('/chapters/getAll', chapters.getAll);
--- Backend/app/validators/chapter.validation.js
+++ Backend/app/validators/chapter.validation.js
@@ -1,4 +1,4 @@
-const { body, param, query } = require('express-validator');
+const { body } = require('express-validator');
 
 const validationSchema = {
     createChapterSchema: [
