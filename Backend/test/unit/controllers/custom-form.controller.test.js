cfc9987e67e2ac62d67489b56d6811397b1559fb
nancytaen <taen.heizo8@gmail.com>
2022-04-23 17:42:09 -0400

    wrote custom form controller tests

---
+++ Backend/test/unit/controllers/custom-form.controller.test.js
@@ -0,0 +1,69 @@
+const { getMockReq, getMockRes } = require('@jest-mock/express');
+const formController = require('../../../app/controllers/custom-form.controller');
+const CustomForm = require('../../../app/models/custom-form.model');
+
+jest.mock('../../../app/models/custom-form.model', () => ({
+  queryForm: jest.fn(),
+  update: jest.fn(),
+  publish: jest.fn(),
+}));
+
+const { res, next, mockClear } = getMockRes();
+const mockError = new Error('error');
+const mockFormParam = { form_id: 1 };
+
+
+describe('\'Custom Form Controller unit tests\' ', () => {
+  beforeEach(() => {
+    mockClear(); // can also use clearMockRes()
+    jest.clearAllMocks();
+  });
+
+  test('\'getCustomForm\' 200', async () => {
+    const req = getMockReq({ params: mockFormParam });
+    const mockResponse = {
+      form_id: 1,
+      title: 'test'
+    };
+    CustomForm.queryForm.mockReturnValue(mockResponse);
+    await formController.getCustomForm(req, res, next);
+    expect(CustomForm.queryForm).toHaveBeenCalledWith(mockFormParam);
+    expect(res.send).toHaveBeenCalledWith(mockResponse);
+  });
+
+  test('\'getCustomForm\' error', async () => {
+    const req = getMockReq({ params: mockFormParam });
+
+    CustomForm.queryForm.mockRejectedValue(mockError);
+    await formController.getCustomForm(req, res, next);
+
+    expect(CustomForm.queryForm).toHaveBeenCalledWith(mockFormParam);
+    expect(res.send).toHaveBeenCalledTimes(0);
+    expect(next).toHaveBeenCalledWith(mockError);
+  });
+
+  test('\'queryAllAdminForms\' 200', async () => {
+    const req = getMockReq();
+    await formController.queryAllAdminForms(req, res, next);
+    expect(CustomForm.queryForm).toHaveBeenCalledTimes(1);
+  });
+
+  test('\'publishForm\' 200', async () => {
+    const req = getMockReq({ body: mockFormParam });
+    await formController.publishForm(req, res, next);
+    expect(CustomForm.publish).toHaveBeenCalledWith(mockFormParam.form_id);
+    expect(res.send).toHaveBeenCalledTimes(1);
+  });
+
+
+  // test('\'updateCustomForm\' 200', async () => {
+  //   const req = getMockReq({ params: mockFormParam, body: mockFormBody });
+  //   await formController.updateCustomForm(req, res, next);
+  //   expect(next).toHaveBeenLastCalledWith(0);
+  //   expect(CustomForm.constructor).toHaveBeenCalledTimes(1);
+  //   expect(CustomForm.update).toHaveBeenCalledTimes(1);
+  //   expect(res.send).toHaveBeenCalledTimes(1);
+  // });
+
+
+});
