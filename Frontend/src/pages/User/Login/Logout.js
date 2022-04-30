a65974b772319e50964fb02041c8a5000add97b2
youssefsoli <youssefdevelops@gmail.com>
2022-04-30 03:49:49 -0600

    Fixed logout duplication

--- Frontend/src/pages/User/Login/Logout.js
+++ Frontend/src/pages/User/Login/Logout.js
@@ -3,27 +3,31 @@ import { useHistory } from 'react-router-dom';
 import { logout, selectLoggedIn } from '../../../redux/userSlice';
 import Typography from '@mui/material/Typography';
 import { persistor } from '../../../redux/store';
+import { useEffect } from "react";
 
 export default function Logout() {
   const dispatch = useDispatch();
   const history = useHistory();
 
   const authLogin = useSelector(selectLoggedIn);
-  if (!authLogin) {
-    alert('User is not logged in.');
-    return;
-  }
 
-  fetch('http://localhost:3000/logout').then((response) => {
-    if (response.status !== 200) {
-      console.warn('Logout error occurred');
+  useEffect( ()=> {
+    if (!authLogin) {
+      alert('User is not logged in.');
+      return;
     }
-    alert('User is logged out.');
-    history.replace('/');
-    persistor.purge().then(r => {
-      dispatch(logout());
+
+    fetch('/logout').then((response) => {
+      if (response.status !== 200) {
+        console.warn('Logout error occurred');
+      }
+      alert('User is logged out.');
+      history.replace('/');
+      persistor.purge().then(r => {
+        dispatch(logout());
+      });
     });
-  });
+  }, []);
 
   return <Typography color="black">Error Logging out..</Typography>;
 }
