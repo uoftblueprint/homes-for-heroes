467075ed55716b64ef111ec8adca74616c1ed708
youssefsoli <youssefdevelops@gmail.com>
2022-05-12 23:02:26 -0600

    Added BACKEND_HOST to environment

--- Frontend/src/setupProxy.js
+++ Frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:3000',
+      target: `http://${ process.env.BACKEND_HOST || 'localhost' }:3000`,
       changeOrigin: true,
     })
   );
