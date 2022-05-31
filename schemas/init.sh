d1909a660f46eca6f2b0b2eb9ed3c917175e09e9
dragonite20 <mesbahrafin15@gmail.com>
2022-05-30 21:14:18 -0700

    fixed mysql

--- schemas/init.sh
+++ schemas/init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 echo "CREATE DATABASE IF NOT EXISTS homes_for_heroes;" >  /mainschema.sql
 echo "USE homes_for_heroes;" >>  /mainschema.sql
 echo "ALTER USER root@localhost IDENTIFIED WITH caching_sha2_password BY '$MYSQL_ROOT_PASSWORD';">>  /mainschema.sql
@@ -10,10 +11,18 @@ echo "FLUSH PRIVILEGES;" >>  /mainschema.sql
 
 echo "USE homes_for_heroes;" >>  /mainschema.sql
 
-
 cat admin_client_schema.sql >> /mainschema.sql
 cat user_schema.sql >> /mainschema.sql
 cat chapters_schema.sql >> /mainschema.sql
 cat custom_form_schemas.sql >> /mainschema.sql
 cat external_relations_schema.sql >> /mainschema.sql
-cat sample_schema.sql >> /mainschema.sql
+
+
+if  [ -d  "/var/lib/mysql"  ]; then 
+    mysqld --initialize
+    mysqld --user=root  --init-file="/mainschema.sql"
+
+else 
+    mysqld  --user=root --init-file="/mainschema.sql"
+
+fi
