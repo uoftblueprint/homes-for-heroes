ea51b10b2253c79753d7ba06d5e57df0feb8295e
python-plusplus <sunghyoun.kim@mail.utoronto.ca>
2022-06-07 17:19:52 -0400

    clean sample schema

--- schemas/source_all.sql
+++ schemas/source_all.sql
@@ -1,13 +1,3 @@
-<<<<<<< Updated upstream
-DROP DATABASE homes_for_heroes;
-source ./schemas/admin_client_schema.sql;
-USE homes_for_heroes;
-source ./schemas/user_schema.sql;
-source ./schemas/chapters_schema.sql;
-source ./schemas/custom_form_schemas.sql;
-source ./schemas/external_relations_schema.sql;
-source ./schemas/sample_schema.sql;
-=======
 source ./admin_client_schema.sql;
 USE homes_for_heroes;
 source ./user_schema.sql;
@@ -15,4 +5,3 @@ source ./chapters_schema.sql;
 source ./custom_form_schemas.sql;
 source ./external_relations_schema.sql;
 source ./sample_schema.sql;
->>>>>>> Stashed changes
