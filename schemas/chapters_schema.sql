8ca89cdeaf50875a85d3cc3d3717feabe1778338
python-plusplus <sunghyoun.kim@mail.utoronto.ca>
2022-06-12 15:39:09 -0400

    this is multiline asdf

--- schemas/chapters_schema.sql
+++ schemas/chapters_schema.sql
@@ -1,6 +1,6 @@
 USE homes_for_heroes;
 
-CREATE TABLE IF NOT EXISTS chapters (
+CREATE TABLE IF NOT EXISTS des (
     chapter_name VARCHAR(255) NOT NULL,
     chapter_id INT NOT NULL AUTO_INCREMENT,
     PRIMARY KEY(chapter_id)
