d94aacc5756c8a90afadfb7cb79cadcf53c7429f
python-plusplus <sunghyoun.kim@mail.utoronto.ca>
2022-06-12 15:44:33 -0400

    important commit

--- schemas/chapters_schema.sql
+++ schemas/chapters_schema.sql
@@ -1,6 +1,6 @@
 USE homes_for_heroes;
 
-CREATE TABLE IF NOT EXISTS des (
+CREATE TABLE IF NOT EXISTS deez (
     chapter_name VARCHAR(255) NOT NULL,
     chapter_id INT NOT NULL AUTO_INCREMENT,
     PRIMARY KEY(chapter_id)
